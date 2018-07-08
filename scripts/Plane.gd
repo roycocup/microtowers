10720bd17cef103171e0ccf545d85bb8d8d1ffd7
Rodrigo Dias <rodrigo.pitta@gmail.com>
2018-07-09 00:56:29 +0100

    more

--- scripts/Plane.gd
+++ scripts/Plane.gd
@@ -2,18 +2,20 @@ extends KinematicBody2D
 
 export (int) var speed = 200
 export (float) var rotation_speed = 1.5
-var velocity = Vector2()
-var rotation_dir = 0
+export (PackedScene) var bullet
+var can_shoot
 
 func _ready():
+	can_shoot = true
 	pass
 
 func fire():
-	pass
+	if can_shoot:
+		can_shoot = false
+		emit_signal('fire', Bullet, $Turret/Muzzle.global_position, dir)
+		#$AnimationPlayer.play('muzzle_flash')
 
 func get_input(delta):
-	rotation_dir = 0
-	velocity = Vector2()
 	if Input.is_key_pressed(KEY_RIGHT):
 		rotate(rotation_speed * delta)
 	if Input.is_key_pressed(KEY_LEFT):
